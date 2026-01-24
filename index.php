f8398f1b9e0bd94f9c45d3c6384f70e5a7bd6bba
liamgen.js <services@liamcharpentier.fr>
2026-01-24 15:05:34 +0100

    fix: cors api

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 ini_set("display_errors", 1);
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
 require_once("tools/DB.php");
 
 require_once(__DIR__."/MainController.php");
@@ -7,12 +14,6 @@ require_once(__DIR__."/../vendor/autoload.php");
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__."/../");
 $dotenv->load();
-
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 $requestMethod = $_SERVER["REQUEST_METHOD"];
 
